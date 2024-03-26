6b27a117abbf23dad814ee25cca9fffe556f9cfb
Leonardo Vergani <vergani@outlook.com.br>
2024-03-25 22:53:19 -0300

    Deixei melhor o calculo de menos

--- aula012.py
+++ aula012.py
@@ -1,5 +1,5 @@
 preco = float(input('Qual é o preço do produto? R$ '))
-desconto = preco*5/100
-desconto = preco - desconto
+desconto = preco - (preco*5/100)
+
 print('Você vai pagar R$ {:.2f} e vamos descontar 5% que ficará no valor de R$ {:.2f}. '.format(preco,desconto))
 print('O total que você vai pagar é R$ {:.2f}'.format(desconto))
